9fb36b878fa55ebd6616f9bcd72e19f5150aa9ee
GivenCingco <givencingco@gmail.com>
2024-09-24 17:59:39 +0200

    Added port 3000 on validate script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -16,12 +16,18 @@ echo "CONTAINER_NAME: $CONTAINER_NAME"
 echo "Pulling image from ECR: $ECR_REPOSITORY:$IMAGE_TAG"
 ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
-docker pull $ECR_REPOSITORY:$IMAGE_TAG
+docker pull $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY:$IMAGE_TAG
 
-# Stop and remove existing container if it exists
-docker stop $CONTAINER_NAME || true
-docker rm $CONTAINER_NAME || true
+# Check if the container exists
+if [ "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]; then
+    # Stop and remove existing container if it exists
+    echo "Stopping and removing existing container: $CONTAINER_NAME"
+    docker stop $CONTAINER_NAME
+    docker rm $CONTAINER_NAME
+else
+    echo "No existing container named $CONTAINER_NAME found"
+fi
 
 # Run the Docker container
 echo "Running Docker container: $CONTAINER_NAME"
-docker run -d -p 80:3000 --name $CONTAINER_NAME $ECR_REPOSITORY:$IMAGE_TAG
+docker run -d -p 80:3000 --name $CONTAINER_NAME $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$ECR_REPOSITORY:$IMAGE_TAG
--- scripts/validate_service.sh
+++ scripts/validate_service.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
+# Retrieve the public IP
+PUBLIC_IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+
+# Check if the public IP was retrieved successfully
+if [ -z "$PUBLIC_IP" ]; then
+  echo "Failed to retrieve public IP."
+  exit 1
+fi
+
 # Check if the application is running by accessing the public IP
-curl -I http://$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4)
+curl -I http://$PUBLIC_IP:3000
 
 if [ $? -ne 0 ]; then
   echo "Application validation failed."
